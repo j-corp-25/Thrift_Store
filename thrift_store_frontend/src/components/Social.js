035bd5134d5b87c979a737b487d646150e921996
Jordy <jordycorporan95@gmail.com>
2023-10-06 19:09:15 -0400

    added a social section

--- thrift_store_frontend/src/components/Social.js
+++ thrift_store_frontend/src/components/Social.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
+import './Social.css'; // Assuming you have a CSS file for styling
 
 const Social = () => {
-  return (
-    <section className='section social-section'>
-
-    <div className="section section-title">Follow us on Our Socials</div>
-    
-    </section>
-
-  )
+    return (
+        <section className='section social-section'>
+            <div className="section section-title">Follow us on Our Socials</div>
+            <div className="social-icons">
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                    <i className="fa fa-facebook-square" aria-hidden="true"></i>
+                </a>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                    <i className="fa fa-twitter-square" aria-hidden="true"></i>
+                </a>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                    <i className="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+            </div>
+        </section>
+    );
 }
 
-export default Social
+export default Social;
