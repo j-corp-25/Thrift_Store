3b9cc51e7d384727ac33454d9542528715cee8d7
Jordy <jordycorporan95@gmail.com>
2023-10-06 23:25:13 -0400

    remove backend since its not needed

--- thrift_store_backend/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :index]
-  end
-end
