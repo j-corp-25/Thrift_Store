58559635739f4be8ff64f162e8c351d263cf7af9
Jordy <jordycorporan95@gmail.com>
2023-10-08 21:42:00 -0400

    going to translate header

--- thrift_store_frontend/src/components/MainContent.js
+++ thrift_store_frontend/src/components/MainContent.js
@@ -5,11 +5,9 @@ import location_image from "../assets/location.jpg";
 import social_media from "../assets/social_media.jpg";
 import Social from "./Social";
 
-
 const MainContent = () => {
   return (
     <section className="section main-section">
-
       <div className="section section-title">About Elizabeths Shop</div>
 
       <div className="main-sub-section">
@@ -17,10 +15,10 @@ const MainContent = () => {
           <div className="text-block-container">
             <h2 className="first-title"> Our Vision</h2>
             <p className="first-summary">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
-              obcaecati impedit nisi esse minima? Distinctio itaque ut possimus
-              quia sed minima quod adipisci, quos blanditiis repellat,
-              accusantium, expedita tempora animi?
+              At Elizabeth's Thrift Store, we envision being the first choice
+              for quality secondhand finds. Our curated selections reflect not
+              just affordability but also our deep commitment to sustainability
+              and community bonding.
             </p>
           </div>
           <div className="image-container">
@@ -33,10 +31,10 @@ const MainContent = () => {
           <div className="text-block-container">
             <h2 className="first-title"> Our Location</h2>
             <p className="first-summary">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
-              obcaecati impedit nisi esse minima? Distinctio itaque ut possimus
-              quia sed minima quod adipisci, quos blanditiis repellat,
-              accusantium, expedita tempora animi?
+              Nestled in the vibrant heart of WoodHaven, Elizabeth's Thrift
+              Store is both a treasure trove and a testament to the community's
+              diverse spirit. Easily accessible by public transport, our doors
+              open to a world of sustainable shopping experiences.
             </p>
           </div>
           <div className="image-container">
@@ -50,22 +48,21 @@ const MainContent = () => {
           <div className="text-block-container">
             <h2 className="first-title"> Our Social Media</h2>
             <p className="first-summary">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
-              obcaecati impedit nisi esse minima? Distinctio itaque ut possimus
-              quia sed minima quod adipisci, quos blanditiis repellat,
-              accusantium, expedita tempora animi?
+              Stay connected with us on social media for the latest on our
+              unique collections, exclusive sales, and events. Beyond shopping,
+              we also share insights into thrifting and sustainable living,
+              celebrating our community's shared values.
             </p>
           </div>
           <div className="image-container">
             <figure>
-              <img src={social_media} loading="lazy" ></img>
+              <img src={social_media} loading="lazy"></img>
             </figure>
           </div>
         </div>
       </div>
-      <Social/>
+      <Social />
     </section>
-
   );
 };
 
