80b83bb94bd082d347c3c9b5853b2e9e3538c279
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2023-11-30 10:54:29 -0500

    Update App.js

--- thrift_store_frontend/src/App.js
+++ thrift_store_frontend/src/App.js
@@ -2,7 +2,7 @@
 import './styles/reset.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+
 import Social from './components/Social';
 import Contact from './components/Contact';
 import StoreMap from './components/StoreMap';
@@ -15,7 +15,7 @@ function App() {
     <MainContent/>
     <Contact/>
     <StoreMap/>
-    // <Footer/>
+
     </div>
   );
 }
